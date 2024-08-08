b9eea18a4d620ff5b309590b33b5c9fc74804599
mymayorkay <mayork2012@gmail.com>
2024-08-08 20:41:35 +0100

    Laravel document upload

--- resources/views/view-images.blade.php
+++ resources/views/view-images.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Uploaded Images</title>
 </head>
 <body>
@@ -10,5 +12,19 @@
             <img src="{{ asset('storage/' . $image->filename) }}" alt="Image" width="300">
         </div>
     @endforeach
+
+
+    <div class="container-fluid mt-3">
+        @foreach ($images->chunk(3) as $chunk)
+            <div class="row">
+                @foreach ($chunk as $image)
+                    <div class="col-md-4">
+                        <p>{{ $image->fullname }}</p> <!-- Display the user's full name -->
+                        <img src="{{ asset('storage/' . $image->filename) }}" alt="Image" class="img-fluid">
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
 </body>
 </html>
